de339074c560b12741ed821ec5a5d0ad2c7cc47f
Erdem <zahrakon06@gmail.com>
2022-10-13 15:19:23 +0300

    🛠️ Done I Guess

--- users/ErdemGKSL/join-guild-notifications/src/guildListener.js
+++ users/ErdemGKSL/join-guild-notifications/src/guildListener.js
@@ -1,22 +1,33 @@
 import { GuildStore } from "@acord/modules/common";
 import { persist } from "@acord/extension";
+import { webpack } from "@acord/modules";
+const GuildNotifyStore = webpack.findByProperties("updateGuildNotificationSettings", "updateGuildAndChannelNotificationSettings");
 export const currentGuildIds = new Set();
 
 export const listener = () => {
-
-  const guildAddListener = () => {
+  console.log(0);
+  const guildAddListener = async () => {
     const nIds = new Set();
+    await wait(10);
     for (let id in GuildStore.getGuilds()) nIds.add(id);
     const guildIds = [...nIds].filter(id => !currentGuildIds.has(id));
-    if (!guildIds?.length) return;
+    if (!guildIds?.length) {
+      if (nIds.size != currentGuildIds.size) {
+        currentGuildIds.clear();
+        nIds.forEach((value) => currentGuildIds.add(value));
+      }
+      return;
+    }
     const data = { /* suppress_everyone: true, suppress_roles: true, */ muted: true };
     for (let key in persist.ghost.settings) if (persist.ghost.settings[key]) data[key] = persist.ghost.settings[key];
     guildIds.forEach((id) => {
-      GuildStore.updateGuildNotificationSettings(id, data);
+      GuildNotifyStore.updateGuildNotificationSettings(id, data);
     });
     currentGuildIds.clear();
     nIds.forEach((value) => currentGuildIds.add(value));
   };
   GuildStore.addReactChangeListener(guildAddListener);
   return () => GuildStore.removeReactChangeListener(guildAddListener);
-}
+};
+
+let wait = (s) => new Promise((resolve => setTimeout(() => resolve(true), s)));
