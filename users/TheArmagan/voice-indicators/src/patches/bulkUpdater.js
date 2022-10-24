99607c6bd0ddbc095cca2a55d668213b9706fc20
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-10-25 02:19:19 +0300

    ASD

--- users/TheArmagan/voice-indicators/src/patches/bulkUpdater.js
+++ users/TheArmagan/voice-indicators/src/patches/bulkUpdater.js
@@ -17,38 +17,40 @@ export function patchBulkUpdater() {
 
                 let currentStates = getAllVoiceStatesEntries();
 
-                let updates = [];
-                let removes = [];
+                // let updates = [];
+                // let removes = [];
 
-                await chillout.forEach(currentStates, (cs)=>{
-                    let found = localCache.lastVoiceStates.find(i=>i[0] === cs[0]);
-                    if (
-                        !found
-                        || !_.isEqual(found[1], cs[1])
-                    ) {
-                        updates.push(cs);
-                    }
-                });
+                // await chillout.forEach(currentStates, (cs)=>{
+                //     let found = localCache.lastVoiceStates.find(i=>i[0] === cs[0]);
+                //     if (
+                //         !found
+                //         || !_.isEqual(found[1], cs[1])
+                //     ) {
+                //         updates.push(cs);
+                //     }
+                // });
                 
-                await chillout.forEach(localCache.lastVoiceStates, (lss)=>{
-                    lss[1].forEach(ls=>{
-                        let css = currentStates.find(i=>i[0] === lss[0]);
-                        if (!css) {
-                            removes.push(ls[0]);
-                        } else {
-                            let cs = css[1].find(cs=>cs[8] === ls[8]);
-                            if (!cs || cs[4] != ls[1][4]) {
-                                removes.push([ls[0], ls[1][4]]);
-                            }
-                        }
-                    }) 
-                });
+                // await chillout.forEach(localCache.lastVoiceStates, (lss)=>{
+                //     lss[1].forEach(ls=>{
+                //         let css = currentStates.find(i=>i[0] === lss[0]);
+                //         if (!css) {
+                //             removes.push(ls[0]);
+                //         } else {
+                //             let cs = css[1].find(cs=>cs[8] === ls[8]);
+                //             if (!cs || cs[4] != ls[1][4]) {
+                //                 removes.push([ls[0], ls[1][4]]);
+                //             }
+                //         }
+                //     }) 
+                // });
 
-                localCache.lastVoiceStates = currentStates;
+                // localCache.lastVoiceStates = currentStates;
 
-                socket.emit("bulkUpdate", [updates, removes]);
+                // socket.emit("bulkUpdate", [updates, removes]);
+                
+                socket.emit("bulkUpdate", currentStates);
 
-                await new Promise(r=>setTimeout(r, persist.ghost.settings.performanceMode ? 2e4 : 5e3));
+                await new Promise(r=>setTimeout(r, persist.ghost.settings.performanceMode ? 20000 : 5000));
                 loop();
             }
 
