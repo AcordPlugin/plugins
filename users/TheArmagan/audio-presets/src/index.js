4b6ed659b798b711cc7a307304add6a7d516a83e
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-10-29 10:11:38 +0300

    new plugin!

---
+++ users/TheArmagan/audio-presets/src/index.js
@@ -0,0 +1,36 @@
+import patchContainer from "./other/patchContainer.js"
+import { persist } from "@acord/extension";
+import patcher from "@acord/patcher";
+import webpack from "@acord/modules/webpack"
+
+const WebSound = webpack.find(i=> i?.prototype?._ensureAudio, true);
+
+export default {
+    load() {
+        patchContainer.add(
+            patcher.before("_ensureAudio", WebSound.prototype, function() {
+                let map = Object.fromEntries(persist.ghost.settings.preset.split(";").map(i=>i.split("=")));
+                if (map[this.name]) {
+                    this.name = map[this.name];
+                }
+            })
+        )
+    },
+    unload() {
+        patchContainer.removeAll();
+    },
+    settings: {
+        data: [
+            {
+                "type": "input",
+                "altType": "text",
+                "property": "preset",
+                "value": "message1=message2;call_ringing=call_ringing_beat;unmute=discodo",
+                "placeholder": "message1=message2;call_ringing=call_ringing_beat;unmute=discodo",
+                "name": "Audio Preset",
+                "description": "Your audio preset configuration..",
+                "size": "large"
+            }
+        ]
+    }
+}
