010303ddc01ce4dfd7bb0d47606f7e7cc948bd24
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-11-07 21:44:03 +0300

    text area support

--- fixtures/acord-ui/src/components/TextArea.jsx
+++ fixtures/acord-ui/src/components/TextArea.jsx
@@ -5,7 +5,7 @@ let inputClasses2 = webpack.findByProps("inputWrapper", "inputDefault");
 let scrollClasses = webpack.findByProps("scrollbarDefault", "scrollbar", "scrollbarGhost");
 
 export function TextArea(props = {}) {
-    return <div className={`${inputClasses2.inputWrapper}`}>
+    return <div className={`${inputClasses2.inputWrapper}`} style={{width: "100%"}}>
         <textarea className={`${inputClasses2.inputDefault} ${inputClasses.textArea} ${scrollClasses.scrollbarDefault}`} {...props}></textarea>
     </div>
 }
