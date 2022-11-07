7773d7252a08fc6107c3565d4d8e49fe8e96df16
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-11-07 13:56:13 +0300

    performance update

--- users/TheArmagan/voice-indicators/src/patches/bulkUpdater.js
+++ users/TheArmagan/voice-indicators/src/patches/bulkUpdater.js
@@ -17,40 +17,22 @@ export function patchBulkUpdater() {
 
                 let currentStates = getAllVoiceStatesEntries();
 
-                // let updates = [];
-                // let removes = [];
+                let updates = [];
 
-                // await chillout.forEach(currentStates, (cs)=>{
-                //     let found = localCache.lastVoiceStates.find(i=>i[0] === cs[0]);
-                //     if (
-                //         !found
-                //         || !_.isEqual(found[1], cs[1])
-                //     ) {
-                //         updates.push(cs);
-                //     }
-                // });
+                await chillout.forEach(currentStates, (cs)=>{
+                    let found = localCache.lastVoiceStates.find(i=>i[0] === cs[0]);
+                    if (
+                        !found
+                        || !_.isEqual(found[1], cs[1])
+                    ) {
+                        updates.push(cs);
+                    }
+                });
                 
-                // await chillout.forEach(localCache.lastVoiceStates, (lss)=>{
-                //     lss[1].forEach(ls=>{
-                //         let css = currentStates.find(i=>i[0] === lss[0]);
-                //         if (!css) {
-                //             removes.push(ls[0]);
-                //         } else {
-                //             let cs = css[1].find(cs=>cs[8] === ls[8]);
-                //             if (!cs || cs[4] != ls[1][4]) {
-                //                 removes.push([ls[0], ls[1][4]]);
-                //             }
-                //         }
-                //     }) 
-                // });
+                if (updates.length) socket.emit("bulkUpdate", updates);
+                localCache.lastVoiceStates = currentStates;
 
-                // localCache.lastVoiceStates = currentStates;
-
-                // socket.emit("bulkUpdate", [updates, removes]);
-                
-                socket.emit("bulkUpdate", currentStates);
-
-                await new Promise(r=>setTimeout(r, persist.ghost.settings.performanceMode ? 20000 : 5000));
+                await new Promise(r=>setTimeout(r, persist.ghost.settings.performanceMode ? 5000 : 1000));
                 loop();
             }
 
@@ -58,6 +40,7 @@ export function patchBulkUpdater() {
 
             return ()=>{
                 STOP = 1;
+                localCache.lastVoiceStates = [];
             }
         })()
     );
