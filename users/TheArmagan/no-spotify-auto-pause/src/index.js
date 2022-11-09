e8df1090198276672ec26ba85a02d6b2634019fd
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-11-09 23:51:03 +0300

    fix the plugin

--- users/TheArmagan/no-spotify-auto-pause/src/index.js
+++ users/TheArmagan/no-spotify-auto-pause/src/index.js
@@ -1,14 +1,18 @@
 import patchContainer from "./other/patchContainer.js"
 import patcher from "@acord/patcher";
+import { logger } from "@acord/utils";
 
 export default {
     load() {
         patchContainer.add(
             patcher.instead(
-                "fetch",
-                window,
+                "open",
+                XMLHttpRequest.prototype,
                 async function(args, instead) {
-                    if (args[0] == "https://api.spotify.com/v1/me/player/pause") return;
+                    if (args[0] == "PUT" && args[1] == "https://api.spotify.com/v1/me/player/pause") {
+                        logger.log(`[No Spotify Auto Pause] Spotify pause blocked.`);
+                        args[1] = "https://discord.com/"; // basically noop the request
+                    }
                     return instead.call(this, ...args);
                 }
             )
