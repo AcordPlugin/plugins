f4322352eb4616ebdc8d87584543d53d5724877a
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-11-16 15:47:06 +0300

    copy colors

--- users/TheArmagan/custom-profile-disabler/src/index.js
+++ users/TheArmagan/custom-profile-disabler/src/index.js
@@ -1,5 +1,8 @@
 import patchContainer from "./other/patchContainer.js"
 import dom from "@acord/dom";
+import { i18n } from "@acord/extension";
+import { copyText } from "@acord/utils";
+import { toasts } from "@acord/ui";
 
 export default {
     load() {
@@ -17,10 +20,18 @@ export default {
 
                     let toAppend = dom.parseHTML(`
                         <div style="${!elm.className.includes("userProfileModalOuter-") ? "padding-left: 12px; " : ""}padding-top: 12px; display: flex; align-items: center; gap: 4px;">
-                            <div style="border-radius: 50%; width: 14px; height: 14px; background-color: ${color1}; border: 1px solid ${isLight ? 'rgba(0,0,0,0.5)' : 'rgba(255,255,255,0.5)'};"></div>
-                            <div style="border-radius: 50%; width: 14px; height: 14px; background-color: ${color2}; border: 1px solid ${isLight ? 'rgba(0,0,0,0.5)' : 'rgba(255,255,255,0.5)'};"></div>
+                            <div class="color" style="cursor: pointer; border-radius: 50%; width: 14px; height: 14px; background-color: ${color1}; border: 1px solid ${isLight ? 'rgba(0,0,0,0.5)' : 'rgba(255,255,255,0.5)'};"></div>
+                            <div class="color" style="cursor: pointer; border-radius: 50%; width: 14px; height: 14px; background-color: ${color2}; border: 1px solid ${isLight ? 'rgba(0,0,0,0.5)' : 'rgba(255,255,255,0.5)'};"></div>
                         </div>
                     `);
+
+                    toAppend.querySelectorAll(".color").forEach((elm)=>{
+                        let color = `#${elm.style.backgroundColor.split("(")[1].split(")")[0].split(",").map(function(x){ x = parseInt(x).toString(16); return (x.length==1) ? "0"+x : x; }).join("")}`;
+                        elm.onclick = ()=>{
+                            toasts.show(i18n.format("X_COPIED", color));
+                            copyText(color);
+                        }
+                    });
 
                     container.prepend(toAppend);
                 }    
