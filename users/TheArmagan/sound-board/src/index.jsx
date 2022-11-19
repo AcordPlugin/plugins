a9915d12f6b1923706bc00a6dea15c76582d059a
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-11-19 22:39:58 +0300

    easly add sounds to soundboard!

--- users/TheArmagan/sound-board/src/index.jsx
+++ users/TheArmagan/sound-board/src/index.jsx
@@ -4,6 +4,9 @@ import { persist } from "@acord/extension";
 import patchSCSS from "./styles.scss"
 import { openModal } from "./other/apis.js";
 import { Modal } from "./components/Modal.jsx";
+import dom from "@acord/dom";
+import webpack from "@acord/modules/webpack";
+import { i18n } from "@acord/extension";
 
 let mem = {
     /** @type {AudioContext} */
@@ -73,19 +76,25 @@ async function playSound(src, volume = 0.5, slice = { begin: 0, end: 1000 }, wan
 
 }
 
-const updateTable = _.debounce((v) => {
-    let sounds = v || persist.ghost?.settings?.sounds || "";
-    mem.buffCache = {};
-    if (!sounds.trim()) {
-        mem.table = {};
-        return;
-    }
-    mem.table = Object.fromEntries(sounds.trim().split("\n").map(i => { 
+function parseSoundsStr(sounds) {
+    sounds = sounds.trim();
+    if (!sounds) return {};
+    return Object.fromEntries(sounds.split("\n").map(i => { 
         let v = i.trim().split(";"); 
         let begin = (Number(v[3]) || 0) == 0 ? 0 : (Number(v[3]) * 1000);
         return v.length < 2 ? null : [v[0], { src: v[1], volume: Number(v[2]) ?? 1, begin, end: (Number(v[3]) || 0) == 0 ? null : begin + (Number(v[4]) * 1000) }] 
     }).filter(i => i));
-});
+}
+
+function updateTable(v)  {
+    let sounds = v || persist.ghost?.settings?.sounds || "";
+    mem.buffCache = {};
+    mem.table = parseSoundsStr(sounds);
+}
+
+function getFileExtension(urlOrFileName = "") {
+    return urlOrFileName.split(/\?|#/)[0].split(".").pop().toLowerCase();
+};
 
 export default {
     load() {
@@ -108,7 +117,64 @@ export default {
             return () => {
                 window.removeEventListener("keyup", onEvent);
             }
-        })())
+        })());
+
+        const metadataClasses = webpack.findByProps("metadataDownload", "wrapper");
+        const anchorClasses = webpack.findByProps("anchor", "anchorUnderlineOnHover");
+        const ALLOWED_EXTS = ["mp3", "wav", "ogg", "m4a"];
+        patchContainer.add(
+            dom.patch(
+                `[class*="anchor-"][class*="anchorUnderlineOnHover-"][class*="metadataDownload-"]`,
+                /** @param {Element} elm */ (elm)=>{
+                    let href = elm.getAttribute("href");
+                    if (!ALLOWED_EXTS.includes(getFileExtension(href))) return;
+
+                    let parent = elm.parentElement;
+
+                    let fileName = `${href.split("/").pop().split(".").slice(0, -1).join(".")}_${href.split("/")[5]}`;
+
+                    /** @type {Element} */
+                    let btn = dom.parseHTML(`
+                        <div class="${anchorClasses.anchor} ${anchorClasses.anchorUnderlineOnHover} ${metadataClasses.metadataDownload} sb--add-sound">
+                            <svg class="${metadataClasses.metadataIcon}" viewBox="0 0 24 24" width="24" height="24">
+                                <path fill="currentColor" d="M12.5,17.6l3.6,2.2a1,1,0,0,0,1.5-1.1l-1-4.1a1,1,0,0,1,.3-1l3.2-2.8A1,1,0,0,0,19.5,9l-4.2-.4a.87.87,0,0,1-.8-.6L12.9,4.1a1.05,1.05,0,0,0-1.9,0l-1.6,4a1,1,0,0,1-.8.6L4.4,9a1.06,1.06,0,0,0-.6,1.8L7,13.6a.91.91,0,0,1,.3,1l-1,4.1a1,1,0,0,0,1.5,1.1l3.6-2.2A1.08,1.08,0,0,1,12.5,17.6Z"></path>
+                            </svg>
+                        </div>
+                    `);
+
+                    let innerSvg = btn.querySelector("svg");
+
+                    function update() {
+                        let isExists = !!mem.table[fileName];
+
+                        if (isExists) {
+                            btn.setAttribute("acord--tooltip-content", i18n.format("REMOVE_FROM_SOUNDBOARD"));
+                            innerSvg.classList.add("exists")
+                        } else {
+                            innerSvg.classList.remove("exists");
+                            btn.setAttribute("acord--tooltip-content", i18n.format("ADD_TO_SOUNDBOARD"));
+                        };
+                    }
+
+                    btn.onclick = ()=>{
+                        let isExists = !!mem.table[fileName];
+                        if (!isExists) {
+                            persist.store.settings.sounds = `${persist.ghost?.settings?.sounds || ""}\n${fileName};${href};1`.trim();
+                        } else {
+                            let soundLines = (persist.ghost?.settings?.sounds || "").split("\n");
+                            let delIdx = soundLines.findIndex(v=>v.trim().split(";")[0] == fileName);
+                            soundLines.splice(delIdx, 1);
+                            persist.store.settings.sounds = soundLines.join("\n");
+                        }
+                        updateTable();
+                        update();
+                    }
+
+                    update();
+                    parent.insertBefore(btn, elm);
+                }
+            )
+        )
     },
     async unload() {
         patchContainer.removeAll();
