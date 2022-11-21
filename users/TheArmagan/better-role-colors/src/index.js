4fcadd26dca0ba844a5157d4f1dc7f84338c1658
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-11-21 22:50:44 +0300

    better look

--- users/TheArmagan/better-role-colors/src/index.js
+++ users/TheArmagan/better-role-colors/src/index.js
@@ -4,19 +4,19 @@ import dom from "@acord/dom";
 import { GuildMemberStore, SelectedGuildStore } from "@acord/modules/common";
 import color from "color";
 
-
-
 export default {
     load() {
         patchContainer.add(
             dom.patch(
-                '[class*="voiceUser-"] > [class*="content-"]',
+                '[class*="voiceUser-"] > [class*="content-"] > [class*="username-"]',
                 (elm)=>{
                     let props = utils.react.getProps(elm, i => i?.guildId);
                     if (!props?.guildId || !props?.user) return;
-                    let member = GuildMemberStore.getMember(props.guildId, props.user.id);;
-                    if (!member?.colorString) return;
-                    elm.style.backgroundColor = color(member.colorString).alpha(0.15).hexa();
+                    let member = GuildMemberStore.getMember(props.guildId, props.user.id);
+                    if (!member?.colorString || member?.colorString == "#ffffff") return;
+                    let l = color(member?.colorString).l();
+                    if (l < 2 || l > 98) return;
+                    elm.style.color = color([color(member.colorString).hue(), 20, 80], "hsv").hexa();
                 }
             )
         );
@@ -28,8 +28,10 @@ export default {
                     let props = utils.react.getProps(elm, i => i?.message);
                     if (!props?.message) return;
                     let member = GuildMemberStore.getMember(SelectedGuildStore.getGuildId(), props.message.author.id);
-                    if (!member?.colorString) return;
-                    elm.style.color = color([color(member.colorString).hue(), 20, 255], "hsv").hexa();
+                    if (!member?.colorString || member?.colorString == "#ffffff") return;
+                    let l = color(member?.colorString).l();
+                    if (l < 2 || l > 98) return;
+                    elm.style.color = color([color(member.colorString).hue(), 20, 100], "hsv").hexa();
                 }
             )
         );
