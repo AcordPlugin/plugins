fabcca6346f19c94aa7ec6c84a7836a625fa0531
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-11-22 18:00:16 +0300

    bb

--- users/TheArmagan/also-known-as/src/methods/getMemberNicks.js
+++
@@ -1,6 +0,0 @@
-import { GuildMemberStore } from "../other/apis";
-
-export function getMemberNicks(guildId) {
-  let members = GuildMemberStore.getMembers(guildId)
-  return members.filter(i => i.nick).map(i => ({ userId: i.userId, name: i.nick, from: "GUILD", fromId: guildId }));
-}
--- users/TheArmagan/also-known-as/src/patches/style.js
+++
@@ -1,6 +0,0 @@
-import patchContainer from "../other/patchContainer.js";
-import styles from "../styles/styles.scss";
-
-export function patchStyles() {
-  patchContainer.add(styles());
-}
