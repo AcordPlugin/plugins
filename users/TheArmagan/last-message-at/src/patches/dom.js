fc0c57fe97609b0af4307d859499cb68f2059cd7
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-01-08 00:16:03 +0300

    a

--- users/TheArmagan/last-message-at/src/patches/dom.js
+++ users/TheArmagan/last-message-at/src/patches/dom.js
@@ -12,10 +12,10 @@ let eyebrowClasses = webpack.findByProps("eyebrow", "display-lg", "display-md");
 let colorClasses2 = webpack.find(i => i?.defaultColor && Object.keys(i).length == 1);
 let titleClasses = webpack.find(i => i?.title && i?.body && Object.keys(i).length == 2);
 
-function formatDateInfo(data) {
-    let date = new Date(data[0]);
+function formatDate(data) {
+    let date = new Date(data);
     let dateText = `${date.toLocaleString(mainI18N.locale, { month: 'short' })} ${(new Date().getDate()).toString().padStart(2, "0")}, ${date.getFullYear()} ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
-    return `${dateText}${data[1] ? ` (${data[1]})` : ""}`;
+    return dateText;
 }
 
 export function patchDOM() {
@@ -38,7 +38,7 @@ export function patchDOM() {
                 let section = dom.parseHTML(`
                     <div class="${sectionClasses.section}">
                         <h2 class="${colorClasses.defaultColor} ${eyebrowClasses.eyebrow} ${colorClasses2.defaultColor} ${titleClasses.title}">${i18n.format("LAST_MESSAGE_AT")}</h2>
-                        <div class="${colorClasses.defaultColor} ${titleClasses.body}" style="user-select: text;">${formatDateInfo(dateInfo)}</div>
+                        <div class="${colorClasses.defaultColor} ${titleClasses.body}" style="user-select: text; width: fit-content;" acord--tooltip-content="${dom.escapeHTML(dateInfo[1])}">${formatDate(dateInfo[0])}</div>
                     </div>
                 `);
 
@@ -67,7 +67,7 @@ export function patchDOM() {
                 `);
 
                 let contentElm = dom.parseHTML(`
-                    <div class="${colorClasses.defaultColor} ${titleClasses.body}" style="margin-bottom: 16px; user-select: text;">${formatDateInfo(dateInfo)}</div>
+                    <div class="${colorClasses.defaultColor} ${titleClasses.body}" style="margin-bottom: 16px; user-select: text; width: fit-content;" acord--tooltip-content="${dom.escapeHTML(dateInfo[1])}">${formatDate(dateInfo[0])}</div>
                 `);
 
                 p.insertBefore(titleElm, p.children[2]);
