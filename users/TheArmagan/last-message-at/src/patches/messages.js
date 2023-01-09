67b2e2a23714c886cecf6e7458061b977a87d2c3
Erdem <zahrakon06@gmail.com>
2023-01-09 23:45:45 +0300

    .

--- users/TheArmagan/last-message-at/src/patches/messages.js
+++ users/TheArmagan/last-message-at/src/patches/messages.js
@@ -9,11 +9,14 @@ export function patchMessages() {
 
         function onMessage({ message }) {
             if (!message.author) return;
+            if (message.type !== 0) return;
             let channel = ChannelStore.getChannel(message.channel_id);
             let guild = GuildStore.getGuild(message.guild_id);
             localCache.updateCache[message.author.id] = [
                 new Date().toISOString(),
-                `${guild ? `${guild.name} > ` : ""}${((channel.isDM() && !channel.isGroupDM()) ? UserStore.getUser(channel.getRecipientId()).tag : channel.name) || [...new Map(channel.recipients.map(i => [i, UserStore.getUser(i)])).values()].filter(i => i).map(i => i.tag).sort((a, b) => a > b).join(", ")}`
+                `${guild ? `${guild.name} > ` : ""}${((channel.isDM() && !channel.isGroupDM()) ? 
+                    (UserStore.getUser(channel.getRecipientId()).tag + ", " + UserStore.getCurrentUser().tag)
+                    : channel.name) || [...new Map(channel.recipients.map(i => [i, UserStore.getUser(i)])).values()].filter(i => i).map(i => i.tag).sort((a, b) => a > b).join(", ")}`
             ];
         }
 
